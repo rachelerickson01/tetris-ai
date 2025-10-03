02dcde450ed84732b81eb377358b19140852cbbb
Rachel Erickson <ericksonrachel2@gmail.com>
2025-10-03 13:05:01 -0600

    added rect class with comments

--- app.js
+++ app.js
@@ -10,3 +10,101 @@ document.addEventListener('DOMContentLoaded', () => {
 
     console.log(squares)
 })
+
+
+
+
+class Rect {
+
+    //constructs a rect given edge locations
+    constructor(l, t, r, b) {
+        this.left = l; // x-coordinate of left edge
+        this.top = t; // y-coordinate of top edge
+        this.right = r; // x-coordinate of right edge
+        this.bottom = b; // y-coordinate of bottom edge
+    }
+
+    toString() {
+        return "[" + this.left + " " + this.top + " " + this.right + " " + this.bottom + "]";
+    }
+
+    // allows for us to return an empty ("zero-area") rect later
+    static makeEmpty() {
+        return new Rect(0, 0, 0, 0);
+    }
+
+    // returns a cloned copy of the given rect
+    clone() {
+        return new Rect(this.left, this.top, this.right, this.bottom);
+    }
+
+    // detects a zero-area or rect with negative edge lengths
+    // origin (0,0) exists at grid's top-left position with all non-negative x, y
+    empty() {
+        return (this.left >= this.right || this.top >= bottom);
+    }
+    
+    // ----------- GEOMETRY HELPER FUNCTIONS -----------
+    width() {
+        return this.right - this.left;
+    }
+    height() {
+        return this.bottom - this.top; 
+    }
+    center() {
+        return new Point((this.left + this.right)/2, (this.top + this.bottom)/2);
+    }
+
+    // ------------- SET RELATIONS FUNCTIONS --------------
+    
+    // returns true if r is in this rect
+    contains(r) {
+
+        if (this.empty() || r.empty()) return false;
+
+        return (this.left <= r.left && this.top <= r.top && this.right >= r.right
+            && this.bottom  >= r.bottom); 
+
+    }
+
+    // returns new rect that is the union of rect and r 
+    // are we always working with rectangles of equal dimensions?? otherwise returned rect may include areas not in union polygon
+    union(r) {
+
+        if (this.empty()) return r.clone(); // union with empty set is equal to the non-empty set
+        if (r.empty()) return this.clone();
+
+        return new Rect(Math.min(this.left, r.left), Math.min(this.top, r.top), 
+                        Math.max(this.right, r.right), Math.max(this.bottom, r.bottom));
+    }
+
+    // returns new rect that is the intersecion of rect and r 
+    intersection(r) {
+
+        if (this.empty() || r.empty()) return Rect.makeEmpty();
+
+        return new Rect(Math.max(this.left, r.left), Math.max(this.top, r.top), 
+                        Math.min(this.right, r.right), Math.min(this.bottom, r.bottom));
+
+    }
+
+    // returns true if intersection is not empty
+    intersects(r) {
+        return !this.intersection(r).empty()
+    }
+
+    // -------------- TRANSLATION/TRANSFORMATION -----------
+    
+    // performs translation given x and y increments 
+    offset(x, y) {
+        this.left += x; this.top +=y; this.right += x; this.bottom += y;
+    }
+
+    // performs relocation to specified location
+    moveTo(x, y) {
+        this.offset(x - this.left, y - this.top) // confused about this logic
+    }
+
+
+
+}   
